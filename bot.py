35640a48a6e9c309a0575ca1695101eed5f8625f
krydie01 <tech-mibs@mail.ru>
2018-08-09 15:40:48 +0900

    second ver

--- bot.py
+++ bot.py
@@ -7,8 +7,11 @@ def start_bot(bot, update):
     mytext = "Привет {}! Я бот городской библиотечной системы, я помогу вам найти книги, посмотреть афишу мероприятий итд".format(update.message.chat.first_name)
     update.message.reply_text(mytext)
 
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
 def main():
-    updtr = Updater('648569211:AAGa4KFOd10w5bao34KCQ5r-SkFiFiNjvHs')
+    updtr = Updater('648569211:AAGa4KFOd10w5bao34KCQ5r-SkFiFiNjvHs', request_kwargs=PROXY)
     updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
     updtr.start_polling()
     updtr.idle()
