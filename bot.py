1eb739b3111359467194cde1672e5d3a50b96546
krydie01 <tech-mibs@mail.ru>
2018-08-09 15:32:51 +0900

    init

---
+++ bot.py
@@ -0,0 +1,17 @@
+import logging
+from telegram.ext import Updater, CommandHandler
+
+logging.basicConfig()
+
+def start_bot(bot, update):
+    mytext = "Привет {}! Я бот городской библиотечной системы, я помогу вам найти книги, посмотреть афишу мероприятий итд".format(update.message.chat.first_name)
+    update.message.reply_text(mytext)
+
+def main():
+    updtr = Updater('648569211:AAGa4KFOd10w5bao34KCQ5r-SkFiFiNjvHs')
+    updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
+    updtr.start_polling()
+    updtr.idle()
+
+if __name__ == "__main__":
+    main()
